a46fd55105a9fe47bb5673a2ebd11fbc13d48a4c
tianyuqi <564417296@qq.com>
2020-09-03 19:40:05 +0800

    将详情页改为functionalComponent

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -1,33 +1,54 @@
-import React, { PureComponent } from "react";
+import React, { useState, useEffect } from "react";
 import { DetailWrapper, Header, Content } from "./style";
 import { connect } from 'react-redux'
-import * as actionCreators from './store/actionCreator'
+// import * as actionCreators from './store/actionCreator'
+import axios from 'axios'
 
-class Detail extends PureComponent {
-  render() {
-    const { title,content } = this.props
-    return (
-      <DetailWrapper>
-        <Header>{title}</Header>
-        <Content dangerouslySetInnerHTML={{__html:content}} />
-      </DetailWrapper>
-    );
-  }
+// class Detail extends PureComponent {
+//   render() {
+//     const { title, content } = this.props
+//     return (
+//       <DetailWrapper>
+//         <Header>{title}</Header>
+//         <Content dangerouslySetInnerHTML={{ __html: content }} />
+//       </DetailWrapper>
+//     );
+//   }
 
-  componentDidMount(){
-    this.props.getDetail(this.props.match.params.id)
-  }
+//   componentDidMount() {
+//     this.props.getDetail(this.props.match.params.id)
+//   }
+// }
+
+function Detail(props) {
+  const id = props.match.params.id;
+  const [info, setInfo] = useState({})
+  useEffect(() => {
+    const getDetail = () => {
+      axios.get("/api/detail.json?id=" + id).then((res) => {
+        setInfo(res.data.data)
+      });
+    }
+    getDetail()
+  }, [id])
+
+  return (
+    <DetailWrapper>
+      <Header>{info.title}</Header>
+      <Content dangerouslySetInnerHTML={{ __html: info.content }} />
+    </DetailWrapper>
+  );
 }
 
-const mapToState = (state)=>({
-  title:state.getIn(['detail','title']),
-  content:state.getIn(['detail','content'])
-})
+// const mapToState = (state) => ({
+//   title: state.getIn(['detail', 'title']),
+//   content: state.getIn(['detail', 'content'])
+// })
 
-const mapToDispatch = (dispatch)=>({
-  getDetail(id){
-    dispatch(actionCreators.getDetail(id))
-  }
-})
+// const mapToDispatch = (dispatch) => ({
+//   getDetail(id) {
+//     dispatch(actionCreators.getDetail(id))
+//   }
+// })
 
-export default connect(mapToState,mapToDispatch)(Detail);
+export default connect(null, null)(Detail);
